ac27e0c0f6c088354eb0a2ec0d4326e3a629d24d
42694647426 <42694647426@users.noreply.github.com>
2023-03-17 12:43:25 -0400

    get filename and line number of destructive wrapping

--- src/main/java/ca/concordia/soen/DestructiveWrapping.java
+++ src/main/java/ca/concordia/soen/DestructiveWrapping.java
@@ -15,57 +15,39 @@ import org.eclipse.jdt.core.dom.CatchClause;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
 
+import ca.concordia.soen.CatchClauseCounter.Visitor;
+
 class DestructiveWrapping {
 
   public static void main(String[]  args) {
-    ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
-    File file = new File(args[0]);
-    List<String> files = new ArrayList<String>();
-    Filewalker.listOfFiles(file, files);
-    for (String filename : files) {
-      String source;
-      try {
-        source = StaticAnalysisDemo.read(filename);
-      } catch (IOException e) {
-        System.err.println(e);
-        continue;
-      }
+	  ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+	    File file = new File(args[0]);
+	    List<String> files = new ArrayList<String>();
+	    Filewalker.listOfFiles(file, files);
+	    for (String filename : files) {
+	      String source;
+	      try {
+	        source = StaticAnalysisDemo.read(filename);
+	      } catch (IOException e) {
+	        System.err.println(e);
+	        continue;
+	      }
 
-      parser.setSource(source.toCharArray());
+	      parser.setSource(source.toCharArray());
 
-      //ASTNode root = parser.createAST(null);
-      final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+	      ASTNode root = parser.createAST(null);
 
-      Visitor visitor = new Visitor();
-      cu.accept(new ASTVisitor() {
-          public boolean visit(CatchClause node) {
-              Statement statement = node.getBody();
-              if (statement instanceof Block) {
-                  Block block = (Block) statement;
-                  for (Object obj : block.statements()) {
-                      if (obj instanceof ThrowStatement) {
-                          ThrowStatement throwStatement = (ThrowStatement) obj;
-                          Expression expr = throwStatement.getExpression();
-                          if (expr instanceof ClassInstanceCreation) {
-                              ClassInstanceCreation instanceCreation = (ClassInstanceCreation) expr;
-                              if (instanceCreation.arguments().isEmpty()) {
-                                  System.out.println("Possible destructive wrapping found at line: "
-                                          + cu.getLineNumber(node.getStartPosition()));
-                              }
-                          }
-                      }
-                  }
-              }
-              return super.visit(node);
-          }
-      });
+	      Visitor visitor = new Visitor();
+	      root.accept(visitor);
 
-      //System.out.println(filename + ": " + visitor.count);
-
-      for (String name : visitor.names) {
-        System.out.println(name);
-      }
-    }
+	      //System.out.println(filename + ": " + visitor.count);
+	      if (visitor.count >0) {
+	      for (String name : visitor.names) {
+	    	System.out.println(filename + ": " + visitor.count);
+	        System.out.println(name);
+	      }
+	      }
+	    }
   }
 
 
@@ -74,10 +56,33 @@ class DestructiveWrapping {
     List<String> names = new ArrayList<>();
 
     @Override
-    public boolean visit(CatchClause clause) {
-      count += 1;
-      names.add(clause.getException().toString());
-      return false;
+    public boolean visit(CatchClause node) {
+    	
+
+                Statement statement = node.getBody();
+                if (statement instanceof Block) {
+                    Block block = (Block) statement;
+                    for (Object obj : block.statements()) {
+                        if (obj instanceof ThrowStatement) {
+                            ThrowStatement throwStatement = (ThrowStatement) obj;
+                            Expression expr = throwStatement.getExpression();
+                            if (expr instanceof ClassInstanceCreation) {
+                                ClassInstanceCreation instanceCreation = (ClassInstanceCreation) expr;
+                                if (instanceCreation.arguments().isEmpty()) {
+                                	count +=1;
+                                	
+                              	  int startLine = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition());
+                              	  int endLine = ((CompilationUnit) node.getRoot()).getLineNumber(node.getStartPosition()+node.getLength());
+                                   String destructive = "Possible destructive wrapping found at line:" + startLine + "," + endLine;
+                                   names.add(destructive);
+                                }
+                            }
+                        }
+                    }
+                }
+                return super.visit(node);
+            
+        
     }
   }
 
